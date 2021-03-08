f432bc818c17a25a41803ccbdf140de4c669599b
pd-mishra <pd.mishra@gmail.com>
2021-03-08 18:52:32 +0530

    donw ch 6 without alerts component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,52 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
+import { AccountService } from './services/account.service';
+import { Stock } from './services/stocks.model';
+import { StocksService } from './services/stocks.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    StocksService
+  ]
 })
 export class AppComponent implements OnInit, OnDestroy {
+  refresh: boolean = true;
+  stocks: Stock[] = [];
+  interval: any;
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private stocksService: StocksService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.accountService.init();
+    this.load();
 
-  }
-
-  ngOnDestroy(): void {
-
+    this.interval = setInterval(() => {
+      if (this.refresh) {
+        this.load();
+      }
+    }, 15000);
   }
 
   toggleRefresh(): void {
+    this.refresh = !this.refresh;
+  }
 
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
   }
 
   reset(): void {
-
+    this.accountService.reset();
   }
 
   private load(): void {
-
+    this.stocksService.getStocks().subscribe(stocks => {
+      this.stocks = stocks;
+    }, error => {
+      console.error(`There was an error loading stocks: ${error}`);
+    });
   }
 }
